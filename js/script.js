caa0734ae3b144581c6665a3893d868f4a7481d4
sudhir pradhan <rcmkkskp@gmail.com>
2020-03-04 10:34:39 +0530

    factory

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 (function(win, $) {
+  var RedCircle = function() {
+      this.item = $('<div class="circle"></div>');
+    },
+    BlueCircle = function() {
+      this.item = $('<div class="circle" style="background:blue"></div>');
+    },
+    CircleFactory = function() {
+      this.create = function(color) {
+        if (color === "blue") {
+          return new BlueCircle();
+        } else {
+          return new RedCircle();
+        }
+      };
+    };
+
   var CircleGeneratorSingleton = (function() {
     var instance;
     function init() {
       var _aCircle = [],
-        _stage = $(".advert");
+        _stage = $(".advert"),
+        _cf = new CircleFactory();
+
       function _position(circle, left, top) {
         circle.css("left", left);
         circle.css("top", top);
       }
 
-      function create(left, top) {
-        var circle = $('<div class="circle"></div>');
+      function create(left, top, color) {
+        var circle = _cf.create(color).item;
         _position(circle, left, top);
         return circle;
       }
@@ -41,7 +59,7 @@
   $(win.document).ready(function() {
     $(".advert").click(function(e) {
       var cg = CircleGeneratorSingleton.getInstance();
-      var circle = cg.create(e.pageX - 25, e.pageY - 25);
+      var circle = cg.create(e.pageX - 25, e.pageY - 25, "red");
 
       cg.add(circle);
     });
@@ -50,7 +68,8 @@
         var cg = CircleGeneratorSingleton.getInstance();
         var circle = cg.create(
           Math.floor(Math.random() * 600),
-          Math.floor(Math.random() * 600)
+          Math.floor(Math.random() * 600),
+          "blue"
         );
 
         cg.add(circle);
