660e42f78983e715200537e20831107c85719e42
sudhir pradhan <rcmkkskp@gmail.com>
2020-03-06 16:15:15 +0530

    abstracting the singleton

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@
     return this.item;
   };
 
-  CircleFactory = function() {
+  ShapeFactory = function() {
     this.types = {};
     this.create = function(type) {
       return new this.types[type]().get();
@@ -69,17 +69,23 @@
     var instance;
     function init() {
       var _aCircle = [],
-        _stage = $(".advert"),
-        _cf = new CircleFactory();
-      _cf.register("red", RedCircleBuilder);
-      _cf.register("blue", BlueCircleBuilder);
+        _stage,
+        _sf = new ShapeFactory();
 
       function _position(circle, left, top) {
         circle.move(left, top);
       }
 
+      function registerShape(name, cls) {
+        _sf.register(name, cls);
+      }
+
+      function setStage(stg) {
+        _stage = stg;
+      }
+
       function create(left, top, type) {
-        var circle = _cf.create(type);
+        var circle = _sf.create(type);
         circle.move(left, top);
         return circle;
       }
@@ -93,7 +99,9 @@
       return {
         index: index,
         create: create,
-        add: add
+        add: add,
+        register: registerShape,
+        setStage: setStage
       };
     }
 
@@ -108,15 +116,17 @@
   })();
 
   $(win.document).ready(function() {
+    var cg = CircleGeneratorSingleton.getInstance();
+    cg.register("red", RedCircleBuilder);
+    cg.register("blue", BlueCircleBuilder);
+    cg.setStage($(".advert"));
     $(".advert").click(function(e) {
-      var cg = CircleGeneratorSingleton.getInstance();
       var circle = cg.create(e.pageX - 25, e.pageY - 25, "red");
 
       cg.add(circle);
     });
     $(win.document).keypress(function(e) {
       if (e.key == "a") {
-        var cg = CircleGeneratorSingleton.getInstance();
         var circle = cg.create(
           Math.floor(Math.random() * 600),
           Math.floor(Math.random() * 600),
